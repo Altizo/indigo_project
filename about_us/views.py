9dfc110198259406557718b7e270185ab9d87a8a
Vlad <integration.into.society@gmail.com>
2019-02-12 21:30:02 +0300

    about page fix

--- about_us/views.py
+++ about_us/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from .models import About_us
 
+
 def about_us(arg):
-    about_us = About_us.objects.values('text')
+    about_us = About_us.objects.values('text').get(pk=1)
     return render(arg, 'about_us.html', {'about_us': about_us})
